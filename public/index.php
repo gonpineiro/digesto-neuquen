04a1bd91d124347699b8f50eaf58c862cc31857f
Gonzalo <gon.pineiro@gmail.com>
2021-10-07 12:15:50 -0300

    :hammer: Estructura

--- public/index.php
+++ public/index.php
@@ -12,18 +12,18 @@ $html = new RenderHtml();
 <div class="container">
     <h1>Digesto</h1>
     <div class="card-container mx-auto justify-content-center">
-        <?= $html->simpleCard('Estatuto', 'Descripción') ?>
         <?= $html->simpleCard('Decretos', 'Descripción') ?>
+        <?= $html->simpleCard('Resoluciones', 'Descripción') ?>
+        <?= $html->simpleCard('Odenanzas', 'Descripción') ?>        
+        <!-- <?= $html->simpleCard('Estatuto', 'Descripción') ?>
         <?= $html->simpleCard('Ediciones Especiales', 'Descripción') ?>
-        <?= $html->simpleCard('Odenanzas', 'Descripción') ?>
         <?= $html->simpleCard('Carta Organica', 'Descripción') ?>
         <?= $html->simpleCard('Disposiciones', 'Descripción') ?>
         <?= $html->simpleCard('Edictos', 'Descripción') ?>
-        <?= $html->simpleCard('Resoluciones', 'Descripción') ?>
         <?= $html->simpleCard('Boletin Oficial', 'Descripción') ?>
         <?= $html->simpleCard('Resoluciones Consejo Deliberante', 'Descripción') ?>
         <?= $html->simpleCard('Reglamento Sindicatura Municipal', 'Descripción') ?>
-        <?= $html->simpleCard('Resolucion IMUH', 'Descripción') ?>
+        <?= $html->simpleCard('Resolucion IMUH', 'Descripción') ?> -->
     </div>
 </div>
 
